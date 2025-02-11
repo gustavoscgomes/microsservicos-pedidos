233b209ec7f29d5915d02803ed6a19d2d773e8ac
Gustavo Gomes <gugaasousa@gmail.com>
2025-02-11 12:06:41 -0300

    feat: iniciar estrutura base do projeto

---
+++ pedidos-api/src/test/java/br/com/gustavoscgomes/pedidos/api/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoscgomes.pedidos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
