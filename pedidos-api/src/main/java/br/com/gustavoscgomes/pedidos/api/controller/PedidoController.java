277a74affadb4e6b7d7951d79b15e4cb4042665b
Gustavo Gomes <gugaasousa@gmail.com>
2025-02-12 15:33:50 -0300

    feat: adicionar e configurar documentacao com swagger

--- pedidos-api/src/main/java/br/com/gustavoscgomes/pedidos/api/controller/PedidoController.java
+++ pedidos-api/src/main/java/br/com/gustavoscgomes/pedidos/api/controller/PedidoController.java
@@ -1,6 +1,12 @@
 package br.com.gustavoscgomes.pedidos.api.controller;
 
 import br.com.gustavoscgomes.pedidos.api.entity.Pedido;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,12 +16,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Pedidos", description = "Recurso para criar um nome pedido")
 @RestController
 @RequestMapping("/api/v1/pedidos")
 public class PedidoController {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
-
+    @Operation(summary = "cria um novo pedido", description = "contem as operacoes para criar um novo pedido",
+            responses = @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class)
+                    )
+            )
+    )
     @PostMapping
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
         logger.info("Pedido recebido: {}", pedido.toString());
